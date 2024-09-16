723d3e6a5c34c4330bc3c4b730257550013540a0
monster-1951 <shivashankarsince19@gmail.com>
2024-09-16 09:57:52 +0530

    Removed blog section currently  , but it will be included in further updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Blogs from "@/components/custom/Blogs";
 import HeroSection from "@/components/custom/HeroSection";
 export default function Home() {
   return (
     <main className="bgImage">
       <HeroSection />
-      <Blogs/>
     </main>
   );
 }
--- src/components/custom/HeroSection.tsx
+++ src/components/custom/HeroSection.tsx
@@ -6,49 +6,58 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div className="heroBgImage glass mt-2 mx-2 border-4 border-black rounded-xl flex flex-1 space-x-4  p-4 h-fit">
-      <div className="h-fit w-full p-3 font-medium ">
-        <div className="text-xl">
-          <h1 className="md:text-2xl p-3 font-extrabold text-xl -mt-4">
-            WHY DO YOU NEED A PROFESSIONAL WEBSITE ?
-          </h1>
-          <div className="p-3 font-semibold md:text-2xl text-lg">
-            <div className="flex">
-              <span>&#x2022; </span>
-              <p>Build Strong Online Presence 🏆</p>
-            </div>
-            <div className="flex">
-              <span>&#x2022; </span>
-              <p>Attract more clients 🧲</p>
-            </div>
-            <div className="flex">
-              <span>&#x2022; </span>
-              <p>Make your business grow 💹</p>
-            </div>
-            <div className="flex justify-start items-center w-full p-3 space-x-3">
-              <Link href={"/ContactShivaShankar"}>
+    <div className=" flex flex-1 space-x-4  p-4 h-[85vh] sm:h-[90vh]">
+      <div className="flex glass h-fit w-fit mx-auto">
+        <div className="h-fit w-full p-3 font-medium">
+          <div className="w-full flex rounded-xl">
+            <Image
+              alt="Hero Image"
+              src={HeroImage}
+              className=" mx-auto h-60 w-[80%] sm:w-[50%] md:hidden rounded-xl py-3"
+            />
+          </div>
+          <div className="text-xl">
+            <h1 className="md:text-2xl p-3 font-extrabold text-xl -mt-4">
+              WHY DO YOU NEED A PROFESSIONAL WEBSITE
+            </h1>
+            <div className="p-3 font-semibold md:text-2xl text-lg">
+              <div className="flex">
+                <span>&#x2022; </span>
+                <p>Build Strong Online Presence 🏆</p>
+              </div>
+              <div className="flex">
+                <span>&#x2022; </span>
+                <p>Attract more clients 🧲</p>
+              </div>
+              <div className="flex">
+                <span>&#x2022; </span>
+                <p>Make your business grow 💹</p>
+              </div>
+              <div className="flex justify-start items-center w-full p-3 space-x-3">
+                <Link href={"/ContactShivaShankar"}>
+                  <Button
+                    variant={"outline"}
+                    className="border-2 w-fit font-semibold hover:font-bold"
+                  >
+                    Contact me
+                  </Button>
+                </Link>
                 <Button
                   variant={"outline"}
                   className="border-2 w-fit font-semibold hover:font-bold"
                 >
-                  Contact me
+                  Know more
                 </Button>
-              </Link>
-              <Button
-                variant={"outline"}
-                className="border-2 w-fit font-semibold hover:font-bold"
-              >
-                Know more
-              </Button>
+              </div>
             </div>
           </div>
         </div>
+        <Image
+          alt="Hero Image"
+          src={HeroImage}
+          className="hidden md:inline sm:h-60 w-[60%] xl:w-[40%]  rounded-xl p-2 mt-5"
+        />
       </div>
-      <Image
-        alt="Hero Image"
-        src={HeroImage}
-        className="hidden sm:inline sm:h-60 w-auto h-20 rounded-xl"
-      />
     </div>
   );
 };

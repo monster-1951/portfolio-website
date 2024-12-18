ca26b7c1efc43978c6bf1ec09e26df0909a5da15
monster-1951 <shivashankarsince19@gmail.com>
2024-12-18 21:12:18 +0530

    deleted constant named a

--- src/components/custom/HeroSection.tsx
+++ src/components/custom/HeroSection.tsx
@@ -5,7 +5,6 @@ import { Button } from "../ui/button";
 import Link from "next/link";
 import MySocialHandles from "./MySocialHandles";
 
-const a = "border border-t-0 border-r-0 border-l-0 border-b-[#ff3131]";
 const HeroText =
   "I craft responsive websites , that drive engagement and growth";
 const Name = {
@@ -70,58 +69,3 @@ const HeroSection = () => {
 
 export default HeroSection;
 
-{
-  /* <div className="flex glass h-fit w-fit mx-auto md:p-8">
-  <div className="h-fit w-full p-3 font-medium">
-    <div className="w-full flex rounded-xl">
-      <Image
-        alt="Hero Image"
-        src={HeroImage}
-        className=" mx-auto h-60 w-[80%] sm:w-[50%] md:hidden rounded-xl py-3"
-      />
-    </div>
-    <div className="text-xl">
-      <h1 className="md:text-2xl p-3 font-extrabold text-xl -mt-4">
-        WHY DO YOU NEED A PROFESSIONAL WEBSITE
-      </h1>
-      <div className="p-3 font-semibold md:text-2xl text-lg">
-        <div className="flex">
-          <span>&#x2022; </span>
-          <p>Build Strong Online Presence 🏆</p>
-        </div>
-        <div className="flex">
-          <span>&#x2022; </span>
-          <p>Attract more clients 🧲</p>
-        </div>
-        <div className="flex">
-          <span>&#x2022; </span>
-          <p>Make your business grow 💹</p>
-        </div>
-        <div className="flex justify-start items-center w-full p-3 space-x-3">
-          <Link href={"/ContactShivaShankar"}>
-            <Button
-              variant={"outline"}
-              className="border-2 w-fit font-semibold hover:font-bold"
-            >
-              Contact me
-            </Button>
-          </Link>
-          <Link href={"/MyWork"}>
-          <Button
-            variant={"outline"}
-            className="border-2 w-fit font-semibold hover:font-bold"
-          >
-            See My work
-          </Button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  </div>
-  <Image
-    alt="Hero Image"
-    src={HeroImage}
-    className="hidden md:inline sm:h-60 w-[60%] xl:w-[40%]  rounded-xl p-2 mt-5"
-  />
-</div> */
-}
